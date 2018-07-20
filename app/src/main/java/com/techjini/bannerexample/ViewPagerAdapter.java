6fc6e6b54259bdb1b1a3ecb942a690b033aed3f5
abhay <abhay.k@techjini.com>
2018-07-21 02:42:59 +0530

    Banner Example

---
+++ app/src/main/java/com/techjini/bannerexample/ViewPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.techjini.bannerexample;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private Integer[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+
+    ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout, null);
+        ImageView imageView;
+        imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
+}
